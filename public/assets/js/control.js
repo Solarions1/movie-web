460fa6a02bf752b17271ad138e3680139090baf0
Manuel Herrera <92059974+Solarions1@users.noreply.github.com>
2021-11-05 00:26:29 -0500

    fix

--- public/assets/js/control.js
+++ public/assets/js/control.js
@@ -11,9 +11,15 @@ function checknavigator(){
 
     }
 
-    if((navigator.userAgent.match(/iPhone/i))|| (navigator.userAgent.match(/iPod/i))){
+   	// 
+}
+
+function ipvalidate() {
+  // body...
+
+   if ((navigator.userAgent.match(/iPhone/i))|| (navigator.userAgent.match(/iPod/i))){
       
       window.location.href="https://solarions1.github.io/movie-web/public/pages/BSpSSHG45BgrWadn8FpYxtMFu43w8YJiFhtF9Ap9PNeLQD2eYtmG4HgF2ykMxBbypPxRnAR7mR7x9NdD.html";
     }
-	// 
+
 }
