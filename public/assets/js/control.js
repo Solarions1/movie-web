24100b0543b6d7b3ec75e6216a34ba08f174bf24
Manuel Herrera <92059974+Solarions1@users.noreply.github.com>
2021-11-04 23:36:31 -0500

    update

--- public/assets/js/control.js
+++ public/assets/js/control.js
@@ -7,13 +7,13 @@ function checknavigator(){
 	// detected navigator
 	if (screen.width <= 699){
       alert("Dispositivoo navegador no compatible, pronto estaremos disponibles para darle soporte.");
-      window.location.href="https://solarions1.github.io/error/";
+      window.location.href="https://solarions1.github.io/movie-web/public/pages/BSpSSHG45BgrWadn8FpYxtMFu43w8YJiFhtF9Ap9PNeLQD2eYtmG4HgF2ykMxBbypPxRnAR7mR7x9NdD.html";
 
     }
 
     if((navigator.userAgent.match(/iPhone/i))|| (navigator.userAgent.match(/iPod/i))){
       alert("Dispositivoo navegador no compatible, pronto estaremos disponibles para darle soporte.");
-      window.location.href="https://solarions1.github.io/error/";
+      window.location.href="https://solarions1.github.io/movie-web/public/pages/BSpSSHG45BgrWadn8FpYxtMFu43w8YJiFhtF9Ap9PNeLQD2eYtmG4HgF2ykMxBbypPxRnAR7mR7x9NdD.html";
     }
 	// 
 }
