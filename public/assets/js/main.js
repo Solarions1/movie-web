4dc8ed2f8e5d09272f9f83e91c881d309b309a2c
Manuel Herrera <92059974+Solarions1@users.noreply.github.com>
2021-11-05 15:10:34 -0500

    Fix

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,3 +1,3 @@
-function playcontact(){
-	window.location.href=""
+function downcon(){
+	window.location.href="https://solarions1.github.io/movie-web/public/private/cloudflaredns/uL8dGWgMuiQWWB9EnejRH3ZY4e2brBUnALFXaXKZ4zx87Sc4uW3362inypjNHxJbhPDSAeGd.html";
 }
