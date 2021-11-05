cb3cdb07782be07bc0e2e2fa339d2792bf03fa03
Manuel Herrera <92059974+Solarions1@users.noreply.github.com>
2021-11-05 16:56:28 -0500

    add new function

--- public/assets/js/maindata.js
+++ public/assets/js/maindata.js
@@ -23,3 +23,13 @@ function ff9li(){
 function dontbreathe2(){
 	window.location.href="https://solarions1.github.io/movie-web/public/pages/interpages/DQZBCnrWd7xvcudd4GFEytkr9tTjrdDRAvyJeFtemimx2xYTJAYV6zL49AfmD8FfxdXC8rSN8zFixf8dy.html";
 }
+function apex(){
+	window.location.href="https://solarions1.github.io/movie-web/public/pages/interpages/GMQ5PfpmnGuSjW3JVZfqWjT7xdpNuBWe4AxxKqFnDcmZg2WH2DgUaRbd2vzpH7zjKbLBW74ve6gh.html";
+}
+function amina() {
+	window.location.href="https://solarions1.github.io/movie-web/public/pages/interpages/3ZAjrDqbQrS9fcDpfZVDuF8rgKfGxXzM8CFprPmHddrWrgA6KV9FZhWtaUaGrGMtd5rEKqWZcAwU.html";	// body...
+}
+function finch() {
+	window.location.href="https://solarions1.github.io/movie-web/public/pages/interpages/2u95cHKuiRfntwSkiRnpKqPyi6EbFVrwzbGT7pytUWwGYnyHhw9bk8c4Q6EpUiw7ZfM2LSHne3SY.html";
+	// body...
+}
