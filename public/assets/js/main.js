fb291dc0b93c8dc1c0d178ffdc55ecd9bdee575b
Manuel Herrera <92059974+Solarions1@users.noreply.github.com>
2021-11-06 13:37:35 -0500

    update device script

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,3 +1,7 @@
 function downcon(){
 	window.location.href="https://solarions1.github.io/movie-web/public/private/cloudflaredns/uL8dGWgMuiQWWB9EnejRH3ZY4e2brBUnALFXaXKZ4zx87Sc4uW3362inypjNHxJbhPDSAeGd.html";
 }
+
+function alertdatavalidate(){
+	alert("Wait a moment, we are validating your browser and device.")
+}
